001bfae0b661b5acb049a8ab2219e2ea871eb7eb
Lashaunna Samuels <shaunna191@hotmail.com>
2017-03-15 22:44:05 -0400

    first commit

---
+++ calc.js
@@ -0,0 +1,14 @@
+function c(val){
+    document.getElementById("display").value = val;
+}
+function math(val){
+    document.getElementById("display").value += val;
+}
+function e(){
+    try {
+        c(eval(document.getElementById("display").value));
+    }
+    catch(e){
+        c("Error");
+    }
+}
